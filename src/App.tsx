eed8d67f92392920637da3088cfec24d305bb466
Viktar Kolasau <v.kolasau@andersenlab.com>
2024-03-28 20:50:52 +0100

    add notifications permission check before requesting the token

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,10 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { onMessageListener, requestForToken } from './firebase'
+import checkNotificationPermissions from './utils/checkNotificationPermissions'
 
 function App() {
-  requestForToken();
+  
   onMessageListener();
   return (
     <>
@@ -18,7 +19,7 @@ function App() {
       </div>
       <p className='test-name' style={{wordBreak: "break-all"}}>Vite + React</p>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {checkNotificationPermissions(requestForToken)}
       </p>
     </>
   )
