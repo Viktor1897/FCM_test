75567360294e572c27463b455f547ecf76822f10
Viktar Kolasau <v.kolasau@andersenlab.com>
2024-03-28 16:58:07 +0100

    aweq

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+importScripts('https://www.gstatic.com/firebasejs/9.6.1/firebase-app-compat.js');
+importScripts("https://www.gstatic.com/firebasejs/9.6.1/firebase-messaging-compat.js");
+
+const firebaseConfig = {
+	apiKey: "AIzaSyBzhop2MeuFaL2uJwp6yQOfVkr7zTj-CGE",
+	authDomain: "nokewtest.firebaseapp.com",
+	projectId: "nokewtest",
+	storageBucket: "nokewtest.appspot.com",
+	messagingSenderId: "394111859121",
+	appId: "1:394111859121:web:df7b5c9f53acd2581c6590",
+};
+firebase.initializeApp(firebaseConfig);
+const isSupported = firebase.messaging.isSupported();
+if (isSupported) {
+	console.log("Firebase Messaging is supported");
+	const messaging = firebase.messaging();
+	// Customize background notification handling here
+	messaging.onBackgroundMessage((payload) => {
+		console.log("Background Message:", payload);
+		const notificationTitle = payload.notification.title;
+		const notificationOptions = {
+			body: payload.notification.body,
+		};
+		self.registration.showNotification(notificationTitle, notificationOptions);
+	});
+
+}
